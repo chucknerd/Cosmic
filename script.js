e0c1c5cde708756f438957b88261c49f3b3c31dd
chucknerd <chucknerd1@gmail.com>
2019-11-02 12:45:01 -0500

    c

--- script.js
+++ script.js
@@ -14,4 +14,34 @@ function addLine(line) {
     let textNode = document.createTextNode(line);
     document.getElementById("consoletext").appendChild(textNode);
 }
+
+$(document).ready(function () {    
+    var newText, originaltext = '';
+
+    $('#editable').html(function () {        
+        if ($.trim($(this).html()) == "") {
+            placehold('editable', 'Please enter a name');
+            originaltext = $.trim($('#editable').html());
+        }
+    });
+    $('#editable').click(function () {        
+        if ($.trim($(this).html()) == originaltext) {
+            placehold('editable', '');
+        } else {
+            placehold('editable', newText);
+        }
+    });
+    $('#editable').blur(function () {         
+        newText = $('#editable').html();               
+        if ($.trim($(this).html()) != originaltext && $.trim($(this).html()) !='') {            
+            placehold('editable', newText);
+        } else {
+            placehold('editable', originaltext);
+        }
+    });
+
+});
+function placehold(id, placeholder) {
+    $("#" + id).html(placeholder);
+}
     
