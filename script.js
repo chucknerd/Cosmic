51f405ba5394655cc1f2f8dc80edd35ad49d4c07
eolculnamo2 <rbertram8@gmail.com>
2019-11-03 12:58:49 -0600

    fix centering; refactoring

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
+const inputElement = document.getElementById("textinput");
+const consoleText = document.getElementById("consoletext");
+
 function checkInput() {
-    let event = window.event || event.which;
+  const event = window.event || event.which;
 
-    if (event.keyCode == 13) {
-        event.preventDefault();
-        addLine(document.getElementById("textinput").value);
-        document.getElementById("textinput").value = "";
-    }
-
-    document.getElementById("textinput").style.height = (document.getElementById("textinput").scrollHeight) + "px";
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    addLine(inputElement.value);
+    inputElement.value = "";
+  }
 }
 
 function addLine(line) {
-    const newLine = document.createElement('DIV');
-    newLine.innerText = line;
-    document.getElementById("consoletext").appendChild(newLine);
+  const newLine = document.createElement("DIV");
+  newLine.innerText = line;
+  consoleText.appendChild(newLine);
+
+  consoleText.scrollTop = consoleText.scrollHeight;
 }
-
-// $(document).ready(function () {    
-//     var newText, originaltext = '';
-
-//     $('#editable').html(function () {        
-//         if ($.trim($(this).html()) == "") {
-//             placehold('editable', 'Please enter a name');
-//             originaltext = $.trim($('#editable').html());
-//         }
-//     });
-//     $('#editable').click(function () {        
-//         if ($.trim($(this).html()) == originaltext) {
-//             placehold('editable', '');
-//         } else {
-//             placehold('editable', newText);
-//         }
-//     });
-//     $('#editable').blur(function () {         
-//         newText = $('#editable').html();               
-//         if ($.trim($(this).html()) != originaltext && $.trim($(this).html()) !='') {            
-//             placehold('editable', newText);
-//         } else {
-//             placehold('editable', originaltext);
-//         }
-//     });
-
-// });
-// function placehold(id, placeholder) {
-//     $("#" + id).html(placeholder);
-// }
