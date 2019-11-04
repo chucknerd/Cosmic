31abfffb8b32ad1bdb034288e2b55a6115bd0349
chucknerd <chucknerd1@gmail.com>
2019-11-04 13:53:58 -0600

    v

--- script.js
+++ script.js
@@ -3,18 +3,6 @@
 const inputElement = document.getElementById("textinput");
 const consoleText = document.getElementById("consoletext");
 
-function checkInput() {
-    if (event.keyCode == 13) {
-      if(event.target.value.toLowerCase() === "start") {
-        addLine("New line message");
-      } else {
-      event.preventDefault();
-      addLine(inputElement.value);
-      }
-      inputElement.value = "";
-    }
-  }
-
 function addLine(line) {
   const newLine = document.createElement("DIV");
   newLine.innerText = line;
@@ -33,7 +21,17 @@ function addLine(line) {
 
 //play game
 
-
+function checkInput() {
+    if (event.keyCode == 13) {
+      if(event.target.value.toLowerCase() === "start") {
+        addLine("You hear an alarm sound as you wake up from, somewhat, of a restful night's sleep. It's about as good as you can get these days. The alarm is from a new message. You're still in your bed and it's morning.");
+      } else {
+      event.preventDefault();
+      addLine(inputElement.value);
+      }
+      inputElement.value = "";
+    }
+  }
 
 
 
