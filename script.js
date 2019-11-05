a334858efff7a710d0ac72fb01ee34c566266d63
chucknerd <chucknerd1@gmail.com>
2019-11-05 06:21:39 -0600

    c

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function addLine(line) {
 function checkInput() {
     if (event.keyCode == 13) {
       if(event.target.value.toLowerCase() === "start") {
+          //you start the game
         addLine("You hear an alarm sound as you wake up from, somewhat, of a restful night's sleep. It's about as good as you can get these days. The alarm is from a new message. You're still in your bed and it's morning.");
       } else {
       event.preventDefault();
@@ -32,6 +33,10 @@ function checkInput() {
       inputElement.value = "";
     }
   }
+
+  
+
+  
 
 
 
