2300a8af4d718b05b827fae1eb0af87bbf0985e9
chucknerd <chucknerd1@gmail.com>
2019-11-07 06:40:44 -0600

    c

--- script.js
+++ script.js
@@ -19,11 +19,22 @@ function addLine(line) {
 //help
     document.getElementById("help") = "";
 
+// game startup, typing start and instrcutions
+function checkInput() {
+  if (event.keyCode == 13) {
+    if(event.target.value.toLowerCase() === "start") {
+        //you start the game
+      addLine("You hear an alarm sound as you wake up from, somewhat, of a restful night's sleep. It's about as good as you can get these days. The alarm is from a new message. You're still in your bed and it's morning.");
+    } else {
+    event.preventDefault();
+    addLine(inputElement.value);
+    }
+    inputElement.value = "";
+  }
+  
+};
+
 //play game
-
-
-// typing start and instrcutions
-
 
 const consoleTextObj = {
   look:
@@ -44,19 +55,7 @@ function enterCommand(text) {
   }
 }
 
-function checkInput() {
-    if (event.keyCode == 13) {
-      if(event.target.value.toLowerCase() === "start") {
-          //you start the game
-        addLine("You hear an alarm sound as you wake up from, somewhat, of a restful night's sleep. It's about as good as you can get these days. The alarm is from a new message. You're still in your bed and it's morning.");
-      } else {
-      event.preventDefault();
-      addLine(inputElement.value);
-      }
-      inputElement.value = "";
-    }
-    
-  };
+
 
 
 
