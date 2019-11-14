63a274b776dc49a782a56f6575a79f30eee89e84
chucknerd <chucknerd1@gmail.com>
2019-11-14 07:06:42 -0600

    c

--- script.js
+++ script.js
@@ -48,9 +48,12 @@ function selectOption(option) {
 //defined textNodes or options the player chooses and what happens next
 
 const textNodes = [
+
+  // starting the game with options to restart
+
   {
     id: 1,
-    text: "Welcome to Cosmic! A text-based adventure game set in the far reaches of space. --Instructions: Refresh your browser at any time to restart the game. Select options to advance in your adventure. Thanks for playing! **Created and owned by Little Guyz Entertainment. Copyright 2019",
+    text: "Welcome to Cosmic! A text-based adventure game set in the far reaches of space. --Instructions: Refresh your browser at any time to restart the game. Select options to advance in your adventure. Thanks for playing! *Created and owned by Little Guyz Entertainment. Copyright 2019",
     options: [
       {
         text: 'START',
@@ -72,12 +75,15 @@ const textNodes = [
         },
     ]
   },
+
+  //player has chosen START the game with options to restart
+
   {
     id: 3,
     text: 'You wake up in your room to the sound of an incoming message.',
     options: [
       {
-        text: 'Check your message.',
+        text: 'Speak command to check messages.',
         nextText: 5
       },
       {
@@ -106,12 +112,14 @@ const textNodes = [
         nextText: 7
       },
       {
-        text: 'IGNORE', 
-        setState: { message: true },
+        text: 'Ignore for now', 
         nextText: 6
       },
     ]
   },
+
+  //player has ignored messages and is laying in the bed in the dark
+
   {
     id:6,
     text: 'You ignore the message. What do you want to do?',
@@ -120,28 +128,149 @@ const textNodes = [
         text: 'Look around',
       nextText: 8
       },
-      {
-        text: 'Eat some food',
-      nextText: 9
-      },
-      {
-        text: 'Get Dressed',
-        setState: { adventureClothes: true },
-      nextText: 10
-      },
-      {
-        text: '(1) UNDREAD MESSAGE',
-      nextText: 7
-      },
     ]
   },
   {
     id: 8,
-    text: "You look around your quarters. It's very dark. You can't see.",
+    text: "You look around your quarters. It's very dark. You can't see. Your personal viewing monitor on your desk, to the left, is blinking with a new message.",
     options: [ 
-      
+      {
+        text: "Speak command to turn light on",
+        nextText: 11
+      },
+      {
+        text: "Speak command to check messages",
+        nextText: 12
+      },
     ]
   },
+
+  //room state for when the player initialy turns the lights on
+
+  {
+    id: 11,
+    text: "You say 'lights on' and the flourescent light illuminates. You see your room in all it's bachelor glory. It's nothing special, just what you can afford and a place to rest your head. It's a single room with a bathroom. There is a main viewing monitor in front of you on the wall. A sink to your right. One closet. The bathroom is beside the sink. You have a fridge and freezer combo in the corner. The door to exit is on your left. You have a desk, to your left, that doubles as a dresser. Your personal viewing monitor sits upon your desk, blinking with a new undread message.",
+    options: [
+      {
+        text: "Look in closet",
+        nextText: 13
+      },
+      {
+        text: "Look in bathroom",
+        nextText: 14
+      },
+      {
+        text: "Check the fridge",
+        nextText: 15
+      },
+      {
+        text: "Speak command to check messages",
+        nextText: 12
+      },
+    ]
+  },
+  {
+    id: 14,
+    text: "The flourescent light illuminates as you open the bathroom door and an exhaust fan turns on. You see a toilet in front of you and a shower to the right.",
+    option: [
+      {
+        text: "leave bathroom",
+        nextText: 20
+      },
+    ]
+  },
+
+  // id 13 for initially looking in closet, equipping and inspecting gear 
+
+  {
+    id: 13,
+    text: "You open the door and a flourescent light automatically illuminates. You see your clothes hanging on a rack. There are boots on the floor. Your utility belt is on a shelf above the rack. There is an open box of charge plugs next to the belt.",
+    options: [
+      {
+        text: "Inspect and equip clothing",
+        setState: {adventureClothes: true}, 
+        nextText: 16
+      },
+      {
+        text: "Inspect and equip utility belt",
+        setState: {utilityBelt: true},
+        nextText: 17
+      },
+      {
+        text: "Inspect charge plug box",
+        nextText: 18
+      },
+      {
+        text: "Leave closet",
+        nextText: 19
+      },
+    ]
+  },
+  {
+    id: 16,
+    text: "You put on a button up, mustard-yellow long-sleeve shirt with the collar flipped up. You tie a black tie, around your neck, that you like to wear loosely just for show. You dawn a navy blue jacket and slacks. You lace up some black boots.",
+    requiredState: (currentState) => currentState.adventureClothes,
+    setState: {adventureClothes: false},
+    options: [
+      {
+        text: "Inspect and equip utility belt",
+        nextText: 17
+      },
+      {
+        text: "Leave closet",
+        nextText: 19
+      },
+    ]
+    
+  },
+
+  // id 21 for closet after player has taken something
+  {
+    id: 21,
+    text: "You're in the closet",
+    options: [ 
+      {
+      text: "Inspect and equip clothing",
+      requiredState: (currentState) => currentState.adventureClothes,
+        nextText: 16 
+      },
+      {
+        text: "Inspect and equip utility belt",
+        requiredState: (currentState) => currentState.utilityBelt,
+        nextText: 17
+      },
+    ]
+  },
+
+  // id 19 for room after player has already turned lights on
+
+  {
+    id: 19,
+    text: "You're in your room",
+    options: [
+      {
+        text: "Look in closet",
+         requiredState: (currentState) => currentState.adventureClothes,
+        requiredState: (currentState) => currentState.utilityBelt,
+        nextText: 13
+      },
+      {
+        text: "Look in bathroom",
+        nextText: 14
+      },
+      {
+        text: "Check the fridge",
+        nextText: 15
+      },
+      {
+        text: "Speak command to check messages",
+        nextText: 12
+      },
+    ]
+  },
+
+  // id 12 for checked message 
+
 
 
 // examples 
